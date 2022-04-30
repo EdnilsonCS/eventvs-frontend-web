c20dc57825de846519aaf49229906ec84aec1a23
ednilsoncs <ednilsonsantos43@gmail.com>
2022-04-30 01:05:28 -0300

    chore: corrigido mensagens de erro no componente de criação e edição de evento

--- src/screens/Producer/Event/Edit/index.tsx
+++ src/screens/Producer/Event/Edit/index.tsx
@@ -25,8 +25,16 @@ const AddEvent: VFC = () => {
     descricao: Yup.string().required('Descrição é um campo obrigatório'),
     statusEvento: Yup.string().required('Status é um campo obrigatório'),
     categoriaId: Yup.string().required('Categoria é um campo obrigatório'),
-    dataHoraFim: Yup.string().required('Data de inicio é um campo obrigatório'),
-    dataHoraInicio: Yup.string().required('Data de fim é um campo obrigatório'),
+    dataHoraFim: Yup.date().required(
+      'Data de encerramento é um campo obrigatório',
+    ),
+    dataHoraInicio: Yup.date().required(
+      'Data de inicio é um campo obrigatório',
+    ),
+    horaDeFim: Yup.date().required(
+      'Hora de encerramento é um campo obrigatório',
+    ),
+    horaDeInicio: Yup.date().required('Hora de inicio é um campo obrigatório'),
     logradouro: Yup.string().required('Logradouro é um campo obrigatório'),
     numero: Yup.number().required('Número é um campo obrigatório'),
     bairro: Yup.string().required('Bairro é um campo obrigatório'),
